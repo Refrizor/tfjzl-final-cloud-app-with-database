e6946e8e28d9cc4149c53f622ba473594c8ab718
Refrizor <retizao.tv@gmail.com>
2026-02-12 14:11:47 -0500

    style: reformat

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -2,20 +2,21 @@ from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
 
+
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
+
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -24,8 +25,10 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
+
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
